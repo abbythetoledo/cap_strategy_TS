b3cd44db3f3981073f409a3fce145344ffb16550
Elyn Abby Toledo <abbydtoledo@gmail.com>
2025-05-19 03:17:00 +0000

    Added Sir Jayvee test code

--- srv/test/test/admin-service.test.ts
+++ srv/test/test/admin-service.test.ts
@@ -1,11 +1,34 @@
 const cds = require('@sap/cds');
 const { GET } = cds.test('serve','AdminService');
-const logger = cds.log("AdminServiceTest")
+const logger = cds.log("AdminServiceTest");
+const path = require("path");
 
-test('ProductCategoryProjection should retrieve categories from table', async () => {
-    const categories = await cds.run(SELECT.from(cds.entities.ProductCategory).columns(["ID", "Name", "Description"]));
-    const { data } = await GET ('/admin/ProductCategoryProjection');
-    logger.info(categories);
+describe('Admin Test Suite', () => {
+    let srv : any;
+ 
+    beforeAll(async () => {
+        try {
+            cds.env.requires.db = { kind: 'sqlite', database: ':memory' };
+            srv = await cds.load(path.join(__dirname + '/../../admin-service.cds')).then(cds.serve)
+            await cds.deploy(path.join(__dirname + '/../../../db'));
+     
+        } catch (error) {
+            logger.error(error);
+        } 
+ 
+ 
+    });
 
-    expect(data.value.length).toBe(categories.length)
+    afterAll(async () => {
+        await cds.shutdown();
+    });
+
+    test('ProductCategoryProjection should retrieve categories from table', async () => {
+        const categories = await cds.run(SELECT.from(cds.entities.ProductCategory).columns(["ID", "Name", "Description"]));
+        const { data } = await GET ('/admin/ProductCategoryProjection');
+        logger.info(categories);
+    
+        expect(data.value.length).toBe(categories.length)
+    });
 });
+
